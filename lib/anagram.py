e88c021793a2c88dd4a0c4be8d68d37ae21d518a
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-09-25 15:57:47 -0400

    completed class methods

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = sorted([l for l in word])
+
+    def match(self, list):
+        new_list = []
+
+        for w in list:
+            if sorted([letter for letter in w]) == self.word:
+                new_list.append(w)
+        
+        return new_list
